8902f5ba7ae42af69cf0d3a6348fabc1ec57a2e0
user <user@usernoMacBook-Air.local>
2024-07-18 18:44:46 +0900

    20240718_code

--- ft_write_number.c
+++ ft_write_number.c
@@ -6,7 +6,7 @@
 /*   By: rohta <rohta@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 12:19:03 by rohta             #+#    #+#             */
-/*   Updated: 2024/07/18 17:40:33 by rohta            ###   ########.fr       */
+/*   Updated: 2024/07/18 18:44:26 by rohta            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,9 @@ static size_t	ft_write_flags(t_params *params, size_t *print_len,
 	}
 	else if (plus_space && 0 <= *number)
 	{
-		if (0 < *print_len)
-		{
-			if (!params->flags->flag_zero)
-				--(*print_len);
-			if ((conv_len + 1) < print_len && 0 < *print_len)
-				*print_len -= (*conv_len + 1);
-			if (*number < 0 && params->flags->flag_minus
-				&& (*conv_len < (size_t)(*params->width)))
-				--(*print_len);
-		}
+		if (((*number < 0 && params->flags->flag_minus)
+				|| (!params->flags->flag_zero)) && (0 < *print_len))
+			--(*print_len);
 		if (params->flags->flag_plus)
 			byte += write(STDOUT_FD, "+", sizeof(char));
 		else
@@ -49,38 +42,53 @@ static size_t	ft_write_flags(t_params *params, size_t *print_len,
 	return (byte);
 }
 
+static	void	ft_print_len(t_params *params, bool plus_space,
+		int number, size_t *print_len)
+{
+	if ((plus_space && number < 0 && 0 < *print_len)
+		|| (plus_space && 0 < *print_len && !params->flags->flag_minus))
+		--(*print_len);
+}
+
+static	int	ft_zero_check(t_params *params, char *c)
+{
+	if (params->flags->flag_zero && *params->precision == NOT_SPEC)
+	{
+		*c = '0';
+		return (1);
+	}
+	return (0);
+}
+
 static size_t	ft_write_int(t_params *params, size_t put_prec,
-		size_t print_len, size_t conv_len, int number)
+		size_t print_len, size_t conv_len)
 {
 	size_t	byte;
 	size_t	i;
 	bool	plus_space;
 	char	c;
+	int		number;
 
 	i = 0;
 	byte = 0;
 	plus_space = (params->flags->flag_plus || params->flags->flag_space);
 	c = ' ';
+	number = ft_atoi(params->converted);
 	if (params->flags->flag_minus)
 	{
 		byte += ft_write_flags(params, &print_len, &conv_len, &number);
 		while (i++ < put_prec && print_len--)
 			byte += write(STDOUT_FD, "0", sizeof(char));
 		byte += write(STDOUT_FD, params->converted, conv_len);
-		if (plus_space && number < 0 && 0 < print_len)
-			--print_len;
+		ft_print_len(params, plus_space, number, &print_len);
 		while (conv_len < print_len--)
 			byte += write(STDOUT_FD, " ", sizeof(char));
 	}
 	else
 	{
-		if (params->flags->flag_zero && *params->precision == NOT_SPEC)
-		{
-			c = '0';
+		if (ft_zero_check(params, &c) == 1)
 			byte += ft_write_flags(params, &print_len, &conv_len, &number);
-		}
-		if (plus_space && 0 < print_len)
-			--print_len;
+		ft_print_len(params, plus_space, number, &print_len);
 		while (put_prec + conv_len < print_len--)
 			byte += write(STDOUT_FD, &c, sizeof(char));
 		if (c == ' ')
@@ -144,5 +152,5 @@ size_t	ft_write_number(t_params *params)
 	else if (number < 0 && ((ssize_t)conv_len - 1 < *params->precision)
 		&& *params->width > *params->precision)
 		++put_prec;
-	return (ft_write_int(params, put_prec, print_len, conv_len, number));
+	return (ft_write_int(params, put_prec, print_len, conv_len));
 }
